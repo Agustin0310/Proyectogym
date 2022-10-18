b47f64e004024c12e0a99adee7b086469dae763d
AgusMontenovo <agumontenovo@gmail.com>
2022-10-18 12:45:13 -0300

    CREACIÓN /RUTINAS/DÍAS | CONFIGURACIÓN PANTALLA AVISOS | CONFIGURACIÓN BÁSICA LOGIN

--- src/app/public/pages/rutina/rutina-routing.module.ts
+++ src/app/public/pages/rutina/rutina-routing.module.ts
@@ -7,7 +7,31 @@ const routes: Routes = [
   {
     path: '',
     component: RutinaPage
+  },  {
+    path: 'lunes',
+    loadChildren: () => import('./lunes/lunes.module').then( m => m.LunesPageModule)
+  },
+  {
+    path: 'martes',
+    loadChildren: () => import('./martes/martes.module').then( m => m.MartesPageModule)
+  },
+  {
+    path: 'miercoles',
+    loadChildren: () => import('./miercoles/miercoles.module').then( m => m.MiercolesPageModule)
+  },
+  {
+    path: 'jueves',
+    loadChildren: () => import('./jueves/jueves.module').then( m => m.JuevesPageModule)
+  },
+  {
+    path: 'viernes',
+    loadChildren: () => import('./viernes/viernes.module').then( m => m.ViernesPageModule)
+  },
+  {
+    path: 'sabado',
+    loadChildren: () => import('./sabado/sabado.module').then( m => m.SabadoPageModule)
   }
+
 ];
 
 @NgModule({
