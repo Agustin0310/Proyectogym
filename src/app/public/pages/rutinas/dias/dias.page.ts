64e4bc9ae7164ee184af35be226390e6f1ec58ff
Gonzalo Bechara <60068962+cacalo@users.noreply.github.com>
2022-10-26 19:52:11 -0300

    Tarjetas de ejericicos por el profe

--- src/app/public/pages/rutinas/dias/dias.page.ts
+++ src/app/public/pages/rutinas/dias/dias.page.ts
@@ -10,10 +10,43 @@ export class DiasPage implements OnInit {
 
   constructor(private ar:ActivatedRoute) { }
 
+  tarjetas = [
+    {
+      musculos: ["piernas","biceps"],
+      ejercicios: [
+        {
+          nombre: "Sentadilla",
+          repeticiones: 20,
+          hecho: false,
+        },
+        {
+          nombre: "Curl de biceps",
+          repeticiones: 10,
+          hecho: true,
+        }
+      ]
+    },
+    {
+      musculos: ["piernas","biceps"],
+      ejercicios: [
+        {
+          nombre: "B",
+          repeticiones: 5555,
+          hecho: false,
+        },
+        {
+          nombre: "A",
+          repeticiones: 10,
+          hecho: true,
+        }
+      ]
+    },
+  ]
+
   ngOnInit() {
     this.ar.params.subscribe (
       (route) => {
-        if(route = ['lunes'])
+        if(route.dia === 'lunes')
         {
           console.log('jaja')
         }
