89e74f3992ad5435acd4a6fc6abbebc66cfe9ae9
AgusMontenovo <agumontenovo@gmail.com>
2022-10-28 12:02:27 -0300

    PROGRESO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,6 @@ export class AppComponent {
   public appPages = [
     { title: 'Home', url: '', icon: 'home' },
     { title: 'Rutinas', url: '/rutinas', icon: 'barbell'},
-    { title: 'Progreso', url: '/progreso', icon: 'checkmark-done' },
     { title: 'Ajustes', url: '/ajustes', icon: 'settings' },
     { title: 'Login', url: '/login', icon: 'finger-print' },
     { title: 'Signin', url: '/signin', icon: 'log-in' },
