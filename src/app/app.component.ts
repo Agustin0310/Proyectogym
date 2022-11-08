4ef001fc0dbb957c6ed1ac5193c85f1349fb79d6
Agustin Maiocco <agustinmaiocco03@gmail.com>
2022-11-07 21:41:30 -0300

    Cambio diseño inputs y otros cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Home', url: '', icon: 'home' },
+    { title: 'Home', url: '', icon: 'home'  },
     { title: 'Rutinas', url: '/rutinas', icon: 'barbell'},
     { title: 'Ajustes', url: '/ajustes', icon: 'settings' },
     { title: 'Login', url: '/login', icon: 'finger-print' },
